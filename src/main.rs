8b949f113127354ce53a35689b5c306681e86d71
Cristian Acuña <c.acuna.sepulveda@gmail.com>
2023-07-27 17:17:31 -0400

    Aplicación simple de pong creada con la ayuda de ChatGPT.
    La aplicación da un error con los "imports" debido al uso de una versión
    antigua de bevy (0.5) que tiene una dependecia rota.

---
+++ src/main.rs
@@ -0,0 +1,97 @@
+use bevy::{prelude::*, input::keyboard::KeyCode};
+
+const PADDLE_SPEED: f32 = 500.0;
+const PADDLE_HEIGHT: f32 = 100.0;
+const PADDLE_WIDTH: f32 = 20.0;
+const BALL_SIZE: f32 = 10.0;
+
+struct Paddle {
+    velocity: Vec3,
+}
+
+struct Ball {
+    velocity: Vec3,
+}
+
+fn setup(mut commands: Commands) {
+    // Spawn paddles
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: Color::WHITE.into(),
+            sprite: Sprite::new(Vec2::new(PADDLE_WIDTH, PADDLE_HEIGHT)),
+            transform: Transform::from_translation(Vec3::new(-400.0, 0.0, 0.0)),
+            ..Default::default()
+        })
+        .insert(Paddle { velocity: Vec3::ZERO });
+
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: Color::WHITE.into(),
+            sprite: Sprite::new(Vec2::new(PADDLE_WIDTH, PADDLE_HEIGHT)),
+            transform: Transform::from_translation(Vec3::new(400.0, 0.0, 0.0)),
+            ..Default::default()
+        })
+        .insert(Paddle { velocity: Vec3::ZERO });
+
+    // Spawn ball
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: Color::WHITE.into(),
+            sprite: Sprite::new(Vec2::new(BALL_SIZE, BALL_SIZE)),
+            transform: Transform::from_translation(Vec3::ZERO),
+            ..Default::default()
+        })
+        .insert(Ball {
+            velocity: Vec3::new(300.0, 150.0, 0.0),
+        });
+}
+
+fn paddle_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&mut Paddle, &Transform)>,
+) {
+    for (mut paddle, transform) in query.iter_mut() {
+        let mut movement = Vec3::ZERO;
+        if keyboard_input.pressed(KeyCode::A) {
+            movement.y -= 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::D) {
+            movement.y += 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::W) {
+            movement.x -= 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::S) {
+            movement.x += 1.0;
+        }
+        if !movement.is_zero() {
+            paddle.velocity = movement.normalize() * PADDLE_SPEED;
+        } else {
+            paddle.velocity = Vec3::ZERO;
+        }
+
+        transform.translation.x = transform.translation.x.min(390.0).max(-390.0);
+        transform.translation.y = transform.translation.y.min(290.0).max(-290.0);
+    }
+}
+
+fn ball_movement(mut query: Query<(&mut Ball, &Transform)>) {
+    for (mut ball, transform) in query.iter_mut() {
+        transform.translation += ball.velocity * 0.02;
+
+        // Reverse ball's direction when hitting the top or bottom of the window
+        if transform.translation.y >= 290.0 || transform.translation.y <= -290.0 {
+            ball.velocity.y *= -1.0;
+        }
+    }
+}
+
+fn main() {
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup.system())
+        .add_system(paddle_movement.system())
+        .add_system(ball_movement.system())
+        .run();
+}
+
